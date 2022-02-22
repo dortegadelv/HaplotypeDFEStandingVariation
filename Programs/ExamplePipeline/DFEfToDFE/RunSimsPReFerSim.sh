73f07930a1324bc68c6e5b9aa96baf7b17b34086
dortegadelv <gochambas@gmail.com>
2022-02-21 20:07:32 -0600

    Corrected PReFerSim run command on RunSimsPReFerSim.sh

--- Programs/ExamplePipeline/DFEfToDFE/RunSimsPReFerSim.sh
+++ Programs/ExamplePipeline/DFEfToDFE/RunSimsPReFerSim.sh
@@ -1,6 +1,6 @@
 Identifier=$1
 PReFerSimFile=$2
 
-time GSL_RNG_SEED=$Identifier GSL_RNG_TYPE=mrg ../PReFerSim/PReFeRSim $PReFerSimFile $Identifier
+time GSL_RNG_SEED=$Identifier GSL_RNG_TYPE=mrg ../PReFerSim/PReFerSim $PReFerSimFile $Identifier
 
 time perl GetListOfRunsWhereFrequencyMatches.pl 0.01 0.01 ../Results/Output.$Identifier.full_out.txt ../Results/Alleles_$Identifier.txt 0
