f826181b0db2f79e2c01989b03e2ff76ed8a9321
dortegadelv <gochambas@gmail.com>
2022-02-25 11:48:41 -0600

    Made CreateDiscreteDFE.sh

--- Programs/ExamplePipeline/CreateDiscreteDFE.sh
+++ Programs/ExamplePipeline/CreateDiscreteDFE.sh
@@ -1,7 +1,11 @@
 cd TableDFE
 
 Rscript CreationOfDiscreteDistribution.R
-perl GetDFETable.pl ../Results/TableToTest.txt ../Results/DFETableToTest.txt
+
+LowNSRow=2
+UpNSRow=202
+
+perl GetDFETable.pl ../Results/TableToTest.txt ../Results/DFETableToTest.txt $LowNSRow $UpNSRow
 
 head -n1 ../Results/DFETableToTest.txt > ../Results/FirstLine.txt
 
